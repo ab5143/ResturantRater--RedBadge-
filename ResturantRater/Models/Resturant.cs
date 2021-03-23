d2b7e3b5dd5c779d990629bab13a8b6cff3bc5a5
atif <ab5143@hotmail.com>
2021-03-23 19:15:23 -0400

    Initial Commit

---
+++ ResturantRater/Models/Resturant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ResturantRater.Models
+{
+    public class Resturant
+    {
+        public int ResturantID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public int Rating { get; set; }
+
+    }
+}
